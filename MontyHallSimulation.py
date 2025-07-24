d393279b90920d0621b83729df412f95c286ba0a
Christopher Duan <62921210+chrislduan@users.noreply.github.com>
2025-07-23 20:55:29 -0500

    Update MontyHallSimulation.py
    Adding user input to the code

--- MontyHallSimulation.py
+++ MontyHallSimulation.py
@@ -2,7 +2,8 @@
 # random numbers
 import random
 
-def monty_game(chances, switch_strategy=True):
+# def monty_game(chances, switch_strategy=True):
+def monty_game(chances):
     
     global goatcount
     global carcount
@@ -17,15 +18,20 @@ def monty_game(chances, switch_strategy=True):
         doors[car_door] = 1
         
         # contestant makes a choice
-        first_choice = random.randint(0,2)
+        # random choice below
+        # first_choice = random.randint(0,2)
+        # choice based on user input
+        first_choice = input() - 1
         
         # monty reveals a goat from a door that hasn't
         # been opened by the contestant
         monty_choice = [x for x in range(3) if x != first_choice and doors[x]==0]
         
         # .choice() method from the random module
-        second_choice = random.choice(monty_choice)
-        
+        # random choice code below
+        # second_choice = random.choice(monty_choice)
+
+        '''
         # switch strategy
         if switch_strategy:
             third_choice = next(x for x in range(3) if x != first_choice and x != second_choice)
@@ -33,7 +39,23 @@ def monty_game(chances, switch_strategy=True):
             if doors[third_choice] == 1:
                 carcount += 1
             else:
-                goatcount +=1
+                goatcount += 1
+        '''
+
+        # user input for second choice, expected input is True or False
+        switch_strategy = input()
+
+        # variable to create final choice, assuming that the user did not switch strategy
+        final_choice = first_choice
+        # if user did choose to switch strategy, then choose the door that is not their original choice nor the one chosen by monty
+        if switch_strategy:
+            final_choice = [x for x in range(3) if x != first_choice and x != monty_choice]
+
+        # score count
+        if doors[final_choice] == 1:
+            carcount += 1
+        else:
+            goatcount += 1
         
     wining_percentage = (carcount/chances)*100
     return wining_percentage
@@ -42,6 +64,7 @@ def monty_game(chances, switch_strategy=True):
 chances = 2
 
 # calls the function monty_game
+# switch_win_percentage = monty_game(chances, switch_strategy=True)
 switch_win_percentage = monty_game(chances, switch_strategy=True)
 
 # f string
