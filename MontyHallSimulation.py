e475d1dad6150891ea3ccef90214e0837c365e4a
rwdsIV <rw4iv123@gmail.com>
2025-07-23 21:13:41 -0500

    Update MontyHallSimulation.py
    Changed Input function to int input and removed switch_strategy

--- MontyHallSimulation.py
+++ MontyHallSimulation.py
@@ -1,5 +1,3 @@
-# in-built module that generates
-# random numbers
 import random
 
 # def monty_game(chances, switch_strategy=True):
@@ -21,47 +19,16 @@ def monty_game(chances):
         # random choice below
         # first_choice = random.randint(0,2)
         # choice based on user input
-        print(f"Choose door 1, 2, or 3")
-        first_choice = input() - 1
+        first_choice = int(input()) - 1
         
         # monty reveals a goat from a door that hasn't
         # been opened by the contestant
         monty_choice = [x for x in range(3) if x != first_choice and doors[x]==0]
-        # print a line to inform user which door was revealed to have a goat
-        print(f"Door {monty_choice + 1} has a goat")
         
         # .choice() method from the random module
         # random choice code below
         # second_choice = random.choice(monty_choice)
-
-        '''
-        # switch strategy
-        if switch_strategy:
-            third_choice = next(x for x in range(3) if x != first_choice and x != second_choice)
-        
-            if doors[third_choice] == 1:
-                carcount += 1
-            else:
-                goatcount += 1
-        '''
-
-        # user input for second choice, expected input is True or False
-        print(f"Do you want to switch your choice, True or False?")
-        switch_strategy = input()
-
-        # variable to create final choice, assuming that the user did not switch strategy
-        final_choice = first_choice
-        # if user did choose to switch strategy, then choose the door that is not their original choice nor the one chosen by monty
-        if switch_strategy:
-            final_choice = [x for x in range(3) if x != first_choice and x != monty_choice]
-
-        # score count
-        if doors[final_choice] == 1:
-            carcount += 1
-        else:
-            goatcount += 1
-        
-    wining_percentage = (carcount/chances)*100
+            wining_percentage = (carcount/chances)*100
     return wining_percentage
 
 # sets the number of iterations to 1000
