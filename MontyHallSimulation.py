f1b05dbdac132d51157ff40e70dc8b4973ae6c41
Christopher Duan <62921210+chrislduan@users.noreply.github.com>
2025-07-23 22:03:06 -0500

    Update MontyHallSimulation.py
    Add print prompts and did testing for simulation

--- MontyHallSimulation.py
+++ MontyHallSimulation.py
@@ -17,6 +17,9 @@ def monty_game(chances):
         car_door = random.randint(0,2)
         doors[car_door] = 1
         
+        # Use for testing, print the doors
+        print(doors)
+        
         # contestant makes a choice
         # random choice below
         # first_choice = random.randint(0,2)
@@ -24,6 +27,7 @@ def monty_game(chances):
         print(f"Choose doors 1-{len(doors)}.")
         # choice based on user input
         first_choice = int(input()) - 1
+        print(f"First Choice: {first_choice}")
         
         # monty reveals a goat from a door that hasn't
         # been opened by the contestant
@@ -31,10 +35,10 @@ def monty_game(chances):
 
         # list of doors monty can open
         monty_doors = [x for x in range(len(doors)) if x != first_choice and doors[x]==0]
-        monty_choice = monty_doors[random.randint(0, len(monty_doors))] + 1
+        monty_choice = monty_doors[random.randint(0, len(monty_doors) - 1)]
 
         # print the door monty reveals
-        print(f"Monty reveals door {monty_choice} has a goat.")
+        print(f"Monty reveals door {monty_choice + 1} has a goat.")
         
         # .choice() method from the random module
         # random choice code below
@@ -54,25 +58,35 @@ def monty_game(chances):
         # prompt if user wants to change their choice
         print(f"Would you like to change your choice, True or False?")
         # user input for second choice, expected input is True or False
-        switch_strategy = input()
+        switch_strategy = False
+        switch_strategy_str = input()
+        if switch_strategy_str == "True" or switch_strategy_str == "T":
+            switch_strategy = True 
+        if switch_strategy_str == "False" or switch_strategy_str == "F":
+            switch_strategy = False 
+        print(f"Switch Strategy: {switch_strategy}")
 
         # variable to create final choice, assuming that the user did not switch strategy
         final_choice = first_choice
         # if user did choose to switch strategy, then choose the door that is not their original choice nor the one chosen by monty
         if switch_strategy:
-            final_choice = [x for x in range(3) if x != first_choice and x != monty_choice]
+            final_choice_doors = [x for x in range(len(doors)) if x != first_choice and x != monty_choice]
+            print(f"Final_choice_doors: {final_choice_doors}")
+            final_choice = final_choice_doors[0]
 
         # score count
+        print(f"Final Choice:{final_choice}")
         if doors[final_choice] == 1:
             carcount += 1
         else:
             goatcount += 1
+        print(f"Car count: {carcount}")
 
     wining_percentage = (carcount/chances)*100
     return wining_percentage
 
 # sets the number of iterations to 1000
-chances = 2
+chances = 1
 
 # calls the function monty_game
 # switch_win_percentage = monty_game(chances, switch_strategy=True)
